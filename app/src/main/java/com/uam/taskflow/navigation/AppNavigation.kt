925806943870ce6ef76f6b742689a228c15b33a0
FranciscoGuevara9128 <fjguevara@uamv.edu.ni>
2026-05-21 17:52:25 -0600

    fix: Corrección imports AppNavigation.kt

--- app/src/main/java/com/uam/taskflow/navigation/AppNavigation.kt
+++ app/src/main/java/com/uam/taskflow/navigation/AppNavigation.kt
@@ -4,10 +4,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import com.uam.taskflow.screen.TaskListScreen
 import com.uam.taskflow.vmodel.TaskViewModel
-
-
 
 @Composable
 fun AppNavigation(modifier: Modifier = Modifier) {
