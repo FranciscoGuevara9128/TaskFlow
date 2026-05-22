39093e071031ba7bb64b9f7db8d972f53e77443e
Oscar Enrrique Arnuero Ramos <oearnuero@uamv.edu.ni>
2026-05-21 18:11:36 -0600

    FEAT

--- app/src/main/java/com/uam/taskflow/navigation/AppNavigation.kt
+++ app/src/main/java/com/uam/taskflow/navigation/AppNavigation.kt
@@ -4,7 +4,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import com.uam.taskflow.vmodel.TaskViewModel
 
 
